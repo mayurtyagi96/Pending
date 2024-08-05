cbaec31748032738ffa4e6249f0986722e84f997
Mayur <mayurtyagi@unolo.com>
2024-08-05 16:39:12 +0530

    Initial Commit

---
+++ Pending/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pending
+//
+//  Created by Mayur  on 05/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pending/PendingApp.swift
@@ -0,0 +1,17 @@
+//
+//  PendingApp.swift
+//  Pending
+//
+//  Created by Mayur  on 05/08/24.
+//
+
+import SwiftUI
+
+@main
+struct PendingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
